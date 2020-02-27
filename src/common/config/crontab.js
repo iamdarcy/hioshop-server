ed65e6fb4749d1fc76047470789021b6841f40fa
shengliang <sligxl@163.com>
2020-02-27 17:15:18 +0800

    停止其中一个定时器

--- src/common/config/crontab.js
+++ src/common/config/crontab.js
@@ -1,10 +1,12 @@
 const path = require('path');
-module.exports = [{
-    interval: '60s',
-    enable: true,
-    immediate: true,
-    handle: "crontab/timetask"
-}, {
+module.exports = [
+// {
+//     interval: '60s',
+//     enable: true,
+//     immediate: true,
+//     handle: "crontab/timetask"
+// }, 
+{
     interval: '10s',
     enable: true,
     immediate: true,
