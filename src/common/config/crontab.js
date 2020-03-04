d40fd42e3479243613deb61245cd05ad7c5f8521
shengliang <sligxl@163.com>
2020-03-04 09:15:56 +0800

    定时任务设置更新下

--- src/common/config/crontab.js
+++ src/common/config/crontab.js
@@ -1,14 +1,15 @@
 const path = require('path');
 module.exports = [
-// {
-//     interval: '60s',
-//     enable: true,
-//     immediate: true,
-//     handle: "crontab/timetask"
-// }, 
 {
-    interval: '10s',
+    interval: '60s',
     enable: true,
     immediate: true,
+    handle: "crontab/timetask"
+}, 
+{
+    interval: '10s',
+    enable: false,
+    immediate: true,
     handle: "crontab/resetSql"
-}]
+}
+]
