cad7ac2893fc0e1b46efad29b8c8294397bd5aaa
shengliang <sligxl@163.com>
2020-04-04 21:45:17 +0800

    修复小程序端的七牛token获取函数的错误

--- src/api/service/qiniu.js
+++ src/api/service/qiniu.js
@@ -1,10 +1,10 @@
 const qiniu = require('qiniu');
 module.exports = class extends think.Service {
     async getQiniuToken() {
-        let accessKey = think.config('qiniu_miniapp.access_key');
-        let secretKey = think.config('qiniu_miniapp.secret_key');
-        let bucket =  think.config('qiniu_miniapp.bucket');
-        let domain =  think.config('qiniu_miniapp.domain');
+        let accessKey = think.config('qiniu.access_key');
+        let secretKey = think.config('qiniu.secret_key');
+        let bucket =  think.config('qiniu.bucket');
+        let domain =  think.config('qiniu.domain');
         let mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
         let currentTime = parseInt(new Date().getTime() / 1000) + 600;
         let key = think.uuid(32);
